37d02241b84319bad1138abb7d07e0a89a58c520
kaiqu <kaiqu@LAPTOP-6RI67E26>
2022-09-21 19:54:24 -0400

    Novo pacote e itens cardapio

---
+++ LanchonetePeixinhoDourado/src/Cardapio/Produto.java
@@ -0,0 +1,31 @@
+package Cardapio;
+
+
+public class Produto {
+    
+    public double preco;
+    public String validade;
+    
+    public Produto (double preco, String validade){
+        this.preco = preco;
+        this.validade = validade;
+    } 
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+    
+    
+}
--- LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/Interface.java
+++ LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/Interface.java
@@ -26,6 +26,19 @@ public class Interface extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 239, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 133, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -78,5 +91,6 @@ public class Interface extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
