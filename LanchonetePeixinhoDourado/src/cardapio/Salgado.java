a13a093979709bfb9434bb136adff9146916af7a
kaiqu <kaiqu@LAPTOP-6RI67E26>
2022-10-01 09:00:25 -0400

    instanciando produtos.

--- LanchonetePeixinhoDourado/src/cardapio/Salgado.java
+++ LanchonetePeixinhoDourado/src/cardapio/Salgado.java
@@ -22,7 +22,7 @@ public class Salgado extends Produto{
     
     @Override
     public String mostrarProduto(){
-        return (nome + " de " + sabor + tipo);
+        return (nome + " de " + sabor + " " + tipo);
     }
     
 }
--- LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/LanchonetePeixinhoDourado.java
+++ LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/LanchonetePeixinhoDourado.java
@@ -1,6 +1,5 @@
 package lanchonetepeixinhodourado;
-import cardapio.Pizza;
-import cardapio.Refrigerante;
+import cardapio.*;
 import pedidos.*;
 
 public class LanchonetePeixinhoDourado {
@@ -9,15 +8,21 @@ public class LanchonetePeixinhoDourado {
         
         Pizza p1 = new Pizza(40, "mussarela", true);
         Refrigerante r1 = new Refrigerante(7, "coca", 500.00);
+        Salgado s1 = new Salgado(10, "salsicha", "frito");
+        Sanduiche san = new Sanduiche(8.99, "presunto e queijo", "massa fina", "maionese");
+        Vitamina vit = new Vitamina(12, "morango", true, 500);
         Item i1 = new Item(p1);
         Item i2 =new Item(r1);
-        Pedido ped = new Pedido("Kirk", FormaDePagamento.dinheiro);
+        Item i3 = new Item(s1);
+        Item i4 = new Item(san);
+        Item i5 = new Item(vit);
+        Pedido ped = new Pedido("Kirk", FormaDePagamento.credito);
         ped.addItem(i1);
         ped.addItem(i2);
-        ped.removeItem(i2);
-        ped.addItem(i1);
-        ped.addItem(i1);
-        ped.addItem(i1);
+        ped.addItem(i3);
+        ped.addItem(i4);
+        ped.addItem(i5);
+        ped.addItem(i5);
         ped.finalizarPedido();
     }   
     
