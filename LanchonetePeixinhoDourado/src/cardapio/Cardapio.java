3b788e53ef6985d94c8e77c1c49bf941e255e229
Lucas Eduardo <110361272+LucasEduardo08@users.noreply.github.com>
2022-10-01 11:48:15 -0400

    Update Cardapio.java

--- LanchonetePeixinhoDourado/src/cardapio/Cardapio.java
+++ LanchonetePeixinhoDourado/src/cardapio/Cardapio.java
@@ -1,5 +1,22 @@
-package cardapio;
+package cardap;
+import java.util.ArrayList;
 
 public class Cardapio {
-    
+    ArrayList<Produto> comidaCardapio = new ArrayList<Produto>();
+
+    public void addProduto(Produto produto){
+        for(int i = 0; i < comidaCardapio.size(); i++){
+            comidaCardapio.add(produto);
+        }
+    }
+
+    public void mostrarCardapio(){
+        for(Produto comida: comidaCardapio){
+            System.out.println(comida.mostrarProduto());
+            // Produto.........Preco
+            // tipos
+        }
+    }
+
+
 }
