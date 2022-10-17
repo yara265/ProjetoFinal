c22d739e68b0622009dc1e47417739585ecc6265
kaiqu <kaiqu@LAPTOP-6RI67E26>
2022-10-16 23:41:37 -0400

    serializando corretamente

--- LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/FinalizarPedido.java
+++ LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/FinalizarPedido.java
@@ -107,7 +107,6 @@ public class FinalizarPedido extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        new FazerPedido(this.pedidoteste, this.s1).setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
--- LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/Vitamina_FazerPedido.java
+++ LanchonetePeixinhoDourado/src/lanchonetepeixinhodourado/Vitamina_FazerPedido.java
@@ -137,7 +137,7 @@ public class Vitamina_FazerPedido extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        new FazerPedido().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
