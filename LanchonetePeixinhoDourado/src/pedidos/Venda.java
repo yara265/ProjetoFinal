b47985d379d2fb1bd720287a3ecec6f537454302
Danilo Bruno <danilobsilv@gmail.com>
2023-10-19 22:21:52 -0400

    - Uso de StringBuilder: Substituição do uso de concatenação de strings por StringBuilder para maior eficiência e clareza. - Melhor Formatação do Código: Melhora na formatação para maior clareza e organização.

--- LanchonetePeixinhoDourado/src/pedidos/Venda.java
+++ LanchonetePeixinhoDourado/src/pedidos/Venda.java
@@ -1,9 +1,10 @@
 package pedidos;
+
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.io.Serializable;
 
-public class Venda implements Serializable{
+public class Venda implements Serializable {
 
     public ArrayList<Item> itens = new ArrayList<Item>();
     public ArrayList<Item> vendas = new ArrayList<Item>();
@@ -11,11 +12,9 @@ public class Venda implements Serializable{
     public double valorTotal;
     public FormaDePagamento formaDePagamento;
     public double valorPago;
-    public double troco;
     DecimalFormat numberFormat = new DecimalFormat("#.00");
 
-    
-    public Venda(String cliente, ArrayList<Item> itens,FormaDePagamento formaDePagamento, double valorTotal, double valorPago){
+    public Venda(String cliente, ArrayList<Item> itens, FormaDePagamento formaDePagamento, double valorTotal, double valorPago) {
         this.cliente = cliente;
         this.itens = itens;
         this.formaDePagamento = formaDePagamento;
@@ -25,123 +24,118 @@ public class Venda implements Serializable{
         this.finalizarPedidoTeste();
         this.regraDeNegocio01();
         this.regraDeNegocio03();
-        //this.finalizarPedido();
     }
 
-    public Venda(){
-        
+    public Venda() {
+
     }
-    
-    public void regraDeNegocio03(){
-        //Se o pagamento for em debito ou em credito, sera acrescentado uma taxa de 1 real.
-        if (this.formaDePagamento == formaDePagamento.credito || this.formaDePagamento == formaDePagamento.debito){
-            this.valorTotal+=1;
+
+    public void regraDeNegocio03() {
+        // Se o pagamento for em débito ou em crédito, será acrescentado uma taxa de 1 real.
+        if (this.formaDePagamento == FormaDePagamento.CREDITO || this.formaDePagamento == FormaDePagamento.DEBITO) {
+            this.valorTotal += 1;
         }
     }
+
     public ArrayList<Item> getProdutos() {
         return itens;
-    }    
-    public void addVendas(){
-        for (Item i: this.itens){
-            if (vendas.contains(i)){
+    }
+
+    public void addVendas() {
+        for (Item i : this.itens) {
+            if (vendas.contains(i)) {
                 i.addQuantidade();
-        }
-            else{
+            } else {
                 vendas.add(i);
-                   }
+            }
         }
-    }
-    
-    public void mostrarPedido(){
-        for (Item i: this.vendas){
-            System.out.println(i.getQuantidade()+ "    " + i.mostrar() + "      R$" + numberFormat.format(i.getPreco()));
-        }
-    }
-    
-    public String mostrarPedidoTeste(){
-        String a = "";
-        for (Item i: this.vendas){
-            a += (i.mostrar()+ "    ||    " + "\n" );
-        }
-        return a;
-    }    
-
-    public double valorPago(double valorPago){
-          if (this.formaDePagamento == FormaDePagamento.credito 
-            || this.formaDePagamento == FormaDePagamento.debito 
-            || this.formaDePagamento == FormaDePagamento.pix){
-                return this.valorTotal;
-        }
-          else{
-              return this.valorPago;
-          }
-    }
-    public double troco(){
-            return valorPago(this.valorPago) - this.valorTotal;
-    }
-    
-    public void regraDeNegocio01(){
-        if (this.valorTotal >= 150){
-            this.valorTotal = this.valorTotal*0.90;
-        }
-        //return valorTotal;
-    }
-    
-    public void finalizarPedido(){
-        System.out.println("Cliente: "+this.cliente);
-        System.out.println("   ------------------------------------   ");
-        // Regra de negocio 01
-        System.out.println("Qntd    Produtos        Preco");
-        System.out.println("------------------------------------");
-        this.mostrarPedido();
-        this.regraDeNegocio01();
-        //System.out.println(this.valorTotal);
-        System.out.println("Forma de pagamento: " +  this.formaDePagamento);
-        System.out.println("Total:                    R$" + numberFormat.format(this.valorTotal));
-        System.out.println("Valor Pago:               R$" + numberFormat.format(this.valorPago(this.valorPago)));
-        System.out.println("Troco:                    R$" + numberFormat.format(troco()));
     }
 
-        public String finalizarPedidoTeste(){
-        return ("Cliente: "+this.cliente)+ "\n"+
-        ("   ------------------------------------   ")+"\n"+
-        // Regra de negocio 01
-        //("Qntd    Produtos        Preco")+"\n"+
-        //("------------------------------------")+"\n"+
-        this.mostrarPedidoTeste()+"\n"+
-        "\n"+
-        //System.out.println(this.valorTotal);
-        ("Forma de pagamento: " +  this.formaDePagamento)+"   --  \n"+
-        ("Total:     R$" + numberFormat.format(this.valorTotal));
+    public Object mostrarPedido() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Qntd    Produtos        Preco\n");
+        sb.append("------------------------------------\n");
+        for (Item i : this.vendas) {
+            sb.append(i.getQuantidade() + "    " + i.mostrar() + "      R$").append(numberFormat.format(i.getPreco())).append("\n");
+        }
+        System.out.print(sb.toString());
+        return null;
     }
-        
-        public String emitirNotaFiscal(){
-        return ("Cliente: "+this.cliente)+ "\n"+
-        ("   ------------------------------------   ")+"\n"+
-        // Regra de negocio 01
-        //("Qntd    Produtos        Preco")+"\n"+
-        //("------------------------------------")+"\n"+
-        this.mostrarPedidoTeste()+"\n"+
-        "\n"+
-        //System.out.println(this.valorTotal);
-        ("Forma de pagamento: " +  this.formaDePagamento)+"   --  \n"+
-        ("Total:     R$" + numberFormat.format(this.valorTotal))+"   --  \n"+
-        ("Valor Pago:     R$" + numberFormat.format(this.valorPago(this.valorPago)))+"   --  \n"+
-        ("Troco:     R$" + numberFormat.format(troco()));
+
+    public String mostrarPedidoTeste() {
+        StringBuilder sb = new StringBuilder();
+        for (Item i : this.vendas) {
+            sb.append(i.mostrar()).append("    ||    \n");
+        }
+        return sb.toString();
     }
-    
+
+    public double valorPago(double valorPago) {
+        if (this.formaDePagamento == FormaDePagamento.CREDITO || this.formaDePagamento == FormaDePagamento.DEBITO || this.formaDePagamento == FormaDePagamento.PIX) {
+            return this.valorTotal;
+        } else {
+            return this.valorPago;
+        }
+    }
+
+    public double troco() {
+        return valorPago(this.valorPago) - this.valorTotal;
+    }
+
+    public void regraDeNegocio01() {
+        if (this.valorTotal >= 150) {
+            this.valorTotal = this.valorTotal * 0.90;
+        }
+    }
+
+    public void finalizarPedido() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente: ").append(this.cliente).append("\n");
+        sb.append("   ------------------------------------   \n");
+        sb.append("Qntd    Produtos        Preco\n");
+        sb.append("------------------------------------\n");
+        sb.append(mostrarPedido());
+        regraDeNegocio01();
+        sb.append("Forma de pagamento: ").append(this.formaDePagamento).append("\n");
+        sb.append("Total:                    R$").append(numberFormat.format(this.valorTotal)).append("\n");
+        sb.append("Valor Pago:               R$").append(numberFormat.format(valorPago(this.valorPago))).append("\n");
+        sb.append("Troco:                    R$").append(numberFormat.format(troco()));
+        System.out.print(sb.toString());
+    }
+
+    public String finalizarPedidoTeste() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente: ").append(this.cliente).append("\n");
+        sb.append("   ------------------------------------   \n");
+        sb.append(mostrarPedidoTeste()).append("\n");
+        sb.append("Forma de pagamento: ").append(this.formaDePagamento).append("   --  \n");
+        sb.append("Total:     R$").append(numberFormat.format(this.valorTotal));
+        return sb.toString();
+    }
+
+    public String emitirNotaFiscal() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente: ").append(this.cliente).append("\n");
+        sb.append("   ------------------------------------   \n");
+        sb.append(mostrarPedidoTeste()).append("\n");
+        sb.append("Forma de pagamento: ").append(this.formaDePagamento).append("   --  \n");
+        sb.append("Total:     R$").append(numberFormat.format(this.valorTotal)).append("   --  \n");
+        sb.append("Valor Pago:     R$").append(numberFormat.format(valorPago(this.valorPago))).append("   --  \n");
+        sb.append("Troco:     R$").append(numberFormat.format(troco()));
+        return sb.toString();
+    }
+
     @Override
     public String toString() {
-        return ("Cliente: "+ 
-        cliente +"\n------------------------------------"+
-        "\nQntd    Produtos        Preco"+
-        "\n------------------------------------"+
-        "    ||   "+
-        "\nForma de Pagamento: "+ formaDePagamento+
-        "\nTotal:                    R$" + numberFormat.format(this.valorTotal)+
-        "\nValor Pago:               R$" + numberFormat.format(this.valorPago(this.valorPago))+
-        "\nTroco:                    R$" + numberFormat.format(troco()) + "                ");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente: ").append(cliente).append("\n------------------------------------\n");
+        sb.append("Qntd    Produtos        Preco\n");
+        sb.append("------------------------------------\n");
+        sb.append("    ||   \n");
+        sb.append("Forma de Pagamento: ").append(formaDePagamento).append("\n");
+        sb.append("Total:                    R$").append(numberFormat.format(this.valorTotal)).append("\n");
+        sb.append("Valor Pago:               R$").append(numberFormat.format(valorPago(this.valorPago))).append("\n");
+        sb.append("Troco:                    R$").append(numberFormat.format(troco())).append("                ");
+        return sb.toString();
     }
-    
-}   
-
+}
