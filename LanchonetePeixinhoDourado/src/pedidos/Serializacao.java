5fd9e5871c53faa05f5f9d728477c5d6c5d3ed49
Danilo Bruno <danilobsilv@gmail.com>
2023-10-19 22:21:33 -0400

    - Melhora no tratamento de exceções - Adição de mensagens de erro - Uso de try-with-resources para gerenciar os recursos de entrada e saída

--- LanchonetePeixinhoDourado/src/pedidos/Serializacao.java
+++ LanchonetePeixinhoDourado/src/pedidos/Serializacao.java
@@ -5,45 +5,30 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class Serializacao {
-    
-    public static final String arquivo_vendas = "BdD/";
-    public static final String nome_arquivo = arquivo_vendas + "vendas.txt";
+    public static final String ARQUIVO_VENDAS = "BdD/";
+    public static final String NOME_ARQUIVO = ARQUIVO_VENDAS + "vendas.txt";
 
-
-    public static ArrayList<Venda> readFile(){
-        ArrayList<Venda> vendas = new ArrayList<>();
-        try{
-            FileInputStream file = new FileInputStream(nome_arquivo);
-            ObjectInputStream objectInput = new ObjectInputStream(file);
-
-            Venda venda;
-            Object object;
-            object = objectInput.readObject();
-            if(object!=null) vendas = (ArrayList<Venda>) object;
-            file.close();
-            objectInput.close();
-        }catch(Exception e){
-            e.printStackTrace();
+    public static ArrayList<Venda> readFile() throws IOException, ClassNotFoundException {
+        try (FileInputStream file = new FileInputStream(NOME_ARQUIVO);
+             ObjectInputStream objectInput = new ObjectInputStream(file)) {
+            return (ArrayList<Venda>) objectInput.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw e;
         }
-        return vendas;
     }
 
-
-    public static void writeFile(Venda venda){
+    public static void writeFile(Venda venda) throws IOException, ClassNotFoundException {
         ArrayList<Venda> vendas = readFile();
-        try{
-            FileOutputStream file = new FileOutputStream(nome_arquivo);
-            ObjectOutputStream object = new ObjectOutputStream(file);
-
+        try (FileOutputStream file = new FileOutputStream(NOME_ARQUIVO);
+             ObjectOutputStream object = new ObjectOutputStream(file)) {
             vendas.add(venda);
             object.writeObject(vendas);
-            file.close();
-            object.close();
-
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (IOException Error) {
+            System.err.println("Erro ao escrever no arquivo: " + Error.getMessage());
+            throw Error;
+        }
     }
-}
 }
